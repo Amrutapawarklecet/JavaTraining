1a6fbd83e2935fb91a744dfc73019dcbb7874b68
Amrutapawarklecet <96716109+Amrutapawarklecet@users.noreply.github.com>
2021-12-27 17:24:42 +0530

    Add files via upload

---
+++ P027/src/com/marlabs/training/Test.java
@@ -0,0 +1,27 @@
+package com.marlabs.training;
+
+public class Test {
+public static void main(String h[]) {
+	String a=h[0];
+	String b=h[1];
+	String c=h[2];
+	
+	int x=Integer.parseInt(a);
+	int y=Integer.parseInt(b);
+	int z=Integer.parseInt(c);
+	int sum=x+y+z;
+	
+	/*float x1=Float.parseFloat(a);
+	float y1=Float.parseFloat(b);
+	float z1=Float.parseFloat(c);
+	float sum1=x1+y1+z1;*/
+
+	/*System.out.println("value of a is: "+a);
+	System.out.println("value of b is: "+b);
+	System.out.println("value of c is: "+c);
+ 	*/
+	System.out.println("Sum is:"+sum);
+	/*System.out.println("Sum1 is:"+sum1);*/
+
+}
+}
