143835cc8c31a2e3d54548f73b13f4cbe6fab697
Amrutapawarklecet <96716109+Amrutapawarklecet@users.noreply.github.com>
2021-12-27 16:55:48 +0530

    Add files via upload

---
+++ P002/src/monday/First.java
@@ -0,0 +1,20 @@
+package monday;
+
+public class First {
+
+	public static void main(String[] args) {
+		byte a=10;
+		short b=20;
+		int c=30;
+		float d=5.6f;
+		long e=40;
+		double f=6.7d;
+		String g="apple";
+	   char h='a';
+	   System.out.println(a+" "+b+" "+c);
+	   System.out.println(d+" "+e+" "+f);
+	   System.out.println(g+" "+h);
+		
+		
+	}
+}
